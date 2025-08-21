bf60f0d31410e33377c378af42f23c74aa8643d1
CHOIisaac <ehdrhkdls@gmail.com>
2025-08-21 22:16:50 +0900

    refactor: Gift 스키마 참조를 CeremonialMoney 스키마로 변경

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,12 +1,12 @@
 # Pydantic 스키마들
 from .user import UserBase, UserCreate, UserUpdate, UserInDB
 from .event import EventBase, EventCreate, EventUpdate, EventInDB
-from .gift import GiftBase, GiftCreate, GiftUpdate, GiftInDB
+from .ceremonial_money import CeremonialMoneyBase, CeremonialMoneyCreate, CeremonialMoneyUpdate, CeremonialMoneyInDB
 from .relationship import RelationshipBase, RelationshipCreate, RelationshipUpdate, RelationshipInDB
 
 __all__ = [
     "UserBase", "UserCreate", "UserUpdate", "UserInDB",
     "EventBase", "EventCreate", "EventUpdate", "EventInDB",
-    "GiftBase", "GiftCreate", "GiftUpdate", "GiftInDB",
+    "CeremonialMoneyBase", "CeremonialMoneyCreate", "CeremonialMoneyUpdate", "CeremonialMoneyInDB",
     "RelationshipBase", "RelationshipCreate", "RelationshipUpdate", "RelationshipInDB",
 ] 
