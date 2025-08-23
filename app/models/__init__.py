c45895b123c513b0ebc42ffc974d02ac35551f21
CHOIisaac <ehdrhkdls@gmail.com>
2025-08-23 19:40:08 +0900

    refactor: Relationship 모델 import 제거

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,5 @@
 from .user import User
 from .event import Event
 from .ceremonial_money import CeremonialMoney
-from .relationship import Relationship
 
-__all__ = ["User", "Event", "CeremonialMoney", "Relationship"] 
+__all__ = ["User", "Event", "CeremonialMoney"] 
