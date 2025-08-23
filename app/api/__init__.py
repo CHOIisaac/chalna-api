c9fb357d1ebace553ef1741a4c1a33da6059ddbe
CHOIisaac <ehdrhkdls@gmail.com>
2025-08-23 19:41:53 +0900

    refactor: relationships_router import 제거

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,12 +3,11 @@ from .auth import router as auth_router
 from .users import router as users_router
 from .events import router as events_router
 from .ceremonial_money import router as ceremonial_money_router
-from .relationships import router as relationships_router
+
 
 __all__ = [
     "auth_router",
     "users_router", 
     "events_router",
     "ceremonial_money_router",
-    "relationships_router",
 ] 
