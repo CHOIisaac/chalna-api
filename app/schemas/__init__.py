45ae52ab67444fcbc7589b1a9ba2299c7b15dfe1
CHOIisaac <ehdrhkdls@gmail.com>
2025-08-24 15:52:12 +0900

    fix: 누락된 user 스키마 import 추가 및 정리

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,12 +1,10 @@
 # Pydantic 스키마들
-from .user import UserBase, UserCreate, UserUpdate, UserInDB
+from .user import UserBase, UserCreate, UserUpdate, UserInDB, UserResponse, UserLogin, Token
 from .event import EventBase, EventCreate, EventUpdate, EventInDB
 from .ceremonial_money import CeremonialMoneyBase, CeremonialMoneyCreate, CeremonialMoneyUpdate, CeremonialMoneyInDB
-from .relationship import RelationshipBase, RelationshipCreate, RelationshipUpdate, RelationshipInDB
 
 __all__ = [
-    "UserBase", "UserCreate", "UserUpdate", "UserInDB",
+    "UserBase", "UserCreate", "UserUpdate", "UserInDB", "UserResponse", "UserLogin", "Token",
     "EventBase", "EventCreate", "EventUpdate", "EventInDB",
     "CeremonialMoneyBase", "CeremonialMoneyCreate", "CeremonialMoneyUpdate", "CeremonialMoneyInDB",
-    "RelationshipBase", "RelationshipCreate", "RelationshipUpdate", "RelationshipInDB",
 ] 
