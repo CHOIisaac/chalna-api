ce01ec1dd3681f8faa30cad70347848537b43b40
CHOIisaac <ehdrhkdls@gmail.com>
2025-09-02 00:08:43 +0900

    feat: 일정 관리 API 모듈 import 추가

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,7 @@ from .auth import router as auth_router
 from .users import router as users_router
 from .events import router as events_router
 from .ceremonial_money import router as ceremonial_money_router
+from .schedules import router as schedules_router
 
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "users_router", 
     "events_router",
     "ceremonial_money_router",
+    "schedules_router",
 ] 
