5b1bdcb6c22e9acd773a2c79a67edbe76ba8792f
CHOIisaac <ehdrhkdls@gmail.com>
2025-09-04 00:19:48 +0900

    refactor: API 패키지에서 ceremonial_money를 ledgers로 변경

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,15 +1,16 @@
-# API 라우터들
-from .auth import router as auth_router
-from .users import router as users_router
-from .events import router as events_router
-from .ceremonial_money import router as ceremonial_money_router
-from .schedules import router as schedules_router
-
+"""
+API 라우터 패키지 초기화
+"""
+from app.api.auth import router as auth_router
+from app.api.users import router as users_router
+from app.api.events import router as events_router
+from app.api.ledgers import router as ledgers_router
+from app.api.schedules import router as schedules_router
 
 __all__ = [
     "auth_router",
     "users_router", 
     "events_router",
-    "ceremonial_money_router",
-    "schedules_router",
+    "ledgers_router",
+    "schedules_router"
 ] 
